685385efb9f7f477bf608c20da531d4e50df6f5a
kiwilvio <qehez11@gmail.com>
2021-02-03 09:35:01 -0800

    temp analysis

--- ARKK.R
+++ ARKK.R
@@ -23,6 +23,7 @@ MF_tickers
 
 getSymbols(MF_tickers, src="yahoo", from = d, to = Sys.Date())
 
+closing <- TSLA$TSLA.Close
 
 for (i in MF_tickers[2:length(MF_tickers)]){
   tryCatch({
@@ -31,4 +32,32 @@ for (i in MF_tickers[2:length(MF_tickers)]){
   closing <- cbind(closing, to_add)
   }, error = function(e){c("ERROR :", conditionMessage(e), "\n")})
 }
-  
+
+# View NA (Unity IPOd recently)
+colSums(is.na(data))
+
+closing <- tibble(date=index(closing), as.tibble(closing))
+
+
+for (i in MF_tickers){
+  tryCatch({
+    to_rename = paste0(i, "_returns")
+    to_add = paste0("(closing$", i, ".Close - lag(closing$", i, ".Close)) / lag(closing$", i, ".Close)")
+    to_add = eval(parse(text = to_add))
+    closing <- cbind(closing, to_add)
+    closing <- rename(closing, !!to_rename := to_add)
+  }, error = function(e){c("ERROR :", conditionMessage(e), "\n")})
+}
+
+closing_prices <- closing[1:46]
+returns <- closing[c(1, 47:91)]
+
+x <- abs((na.fill0(slice(closing_prices[-c(1)], 171), 0) - na.fill0(slice(closing_prices[-c(1)], 1), 0))) / na.fill0(slice(closing_prices[-c(1)], 1), 0)
+
+x <- x %>%
+  tibble::rownames_to_column() %>%  
+  pivot_longer(-rowname) %>% 
+  pivot_wider(names_from=rowname, values_from=value) 
+
+x <- x %>% arrange(desc(`1`))
+x
